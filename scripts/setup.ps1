b321c231f42bafae504894f271a4585a1a6925de
Tomislaw <tomislaw2.0@gmail.com>
2024-08-09 13:20:26 +0200

    Added option to specify MODEL_CACHE_DIR
    Signed-off-by: Tomislaw <tomislaw2.0@gmail.com>

--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -15,9 +15,17 @@ Copy-Item -Path ".env.template" -Destination ".env"
 # Ask user for root directory
 $rootDir = Read-Host "Please enter the root directory path"
 
+# Ask user for model directory
+$modelDir = Read-Host "Please type the model directory path or press enter to use default value ($rootDir/models_cache)"
+# if empty replace with default value
+if ($modelDir -eq "") {
+    $modelDir = "$rootDir/models_cache"
+}
+
 # Update .env file with root directory
 $envContent = Get-Content -Path ".env"
 $envContent = $envContent -replace "ROOT_DIR=.*", "ROOT_DIR=$rootDir"
+$envContent = $envContent -replace "MODEL_CACHE_DIR=.*", "MODEL_CACHE_DIR=$modelDir"
 Set-Content -Path ".env" -Value $envContent
 
 Write-Host ".env file has been created/updated with the provided root directory."
