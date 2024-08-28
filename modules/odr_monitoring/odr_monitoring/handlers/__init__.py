a017ae8aa1dc08d55bddeafd9dbe74a65212a50b
Robert Cronin <robert.cronin@uqconnect.edu.au>
2024-08-28 19:25:18 +1000

    Feature: add odr_monitoring module
    Signed-off-by: Robert Cronin <robert.cronin@uqconnect.edu.au>

---
+++ modules/odr_monitoring/odr_monitoring/handlers/__init__.py
@@ -0,0 +1,4 @@
+from .console_handler import get_console_handler
+from .file_handler import get_file_handler
+
+__all__ = ['get_console_handler', 'get_file_handler']
---
+++ modules/odr_monitoring/odr_monitoring/handlers/file_handler.py
@@ -0,0 +1,26 @@
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+from odr_monitoring.config import logging_config
+from odr_monitoring.formatters.standard_formatter import StandardFormatter
+
+
+class FileHandler(RotatingFileHandler):
+    def __init__(self):
+        log_dir = os.path.dirname(logging_config.LOG_FILE_PATH)
+        os.makedirs(log_dir, exist_ok=True)
+        super().__init__(
+            filename=logging_config.LOG_FILE_PATH,
+            maxBytes=logging_config.LOG_FILE_MAX_BYTES,
+            backupCount=logging_config.LOG_FILE_BACKUP_COUNT
+        )
+        self.setLevel(logging.DEBUG)
+        self.setFormatter(StandardFormatter(
+            fmt=logging_config.LOG_FORMAT,
+            datefmt=logging_config.LOG_DATE_FORMAT,
+            use_colors=False
+        ))
+
+
+def get_file_handler():
+    return FileHandler()
