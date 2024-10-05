5d71940ea75406b1c5cb6bd7941682b16dd3930e
Zachary Licastro <CheesyLaZanya@proton.me>
2024-10-05 15:18:22 -0400

    Allow Playwright job to fail until things are more settled and update configuration for reporters and to avoid potential .only concerns.
    Signed-off-by: Zachary Licastro <CheesyLaZanya@proton.me>

--- modules/odr_frontend/playwright.config.ts
+++ modules/odr_frontend/playwright.config.ts
@@ -2,11 +2,16 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
 	testDir: 'tests',
+	forbidOnly: !!process.env.CI,
 	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
 	use: {
 		baseURL: 'http://localhost:5173',
 		headless: true
-	}
+	},
+	reporter: [
+        ['html', { outputFolder: 'playwright-report' }],
+        ['list']
+    ]
 };
 
 export default config;
