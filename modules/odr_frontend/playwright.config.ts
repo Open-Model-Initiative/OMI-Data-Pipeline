ce771bc466115d939c56c81877bb988406f1255a
Zachary Licastro <CheesyLaZanya@proton.me>
2024-10-05 13:50:42 -0400

    Try creating temporary .env files for pipeline environment.
    Signed-off-by: Zachary Licastro <CheesyLaZanya@proton.me>

--- modules/odr_frontend/playwright.config.ts
+++ modules/odr_frontend/playwright.config.ts
@@ -1,10 +1,6 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
-	// webServer: {
-	// 	command: 'npm run build && npm run preview',
-	// 	port: 4173
-	// },
 	testDir: 'tests',
 	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
 	use: {
