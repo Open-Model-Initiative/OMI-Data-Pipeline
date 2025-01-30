e578fb6c827c8bd6524f639ad623c573dca93dcd
Tomislaw <tomislaw2.0@gmail.com>
2025-01-31 00:36:49 +0100

    Created basic configuration for mounting s3 as volume. Fixed some issues with IAC sub project.

--- cdk/cdk/s3_stack.py
+++ cdk/cdk/s3_stack.py
@@ -1,22 +1,16 @@
 # SPDX-License-Identifier: Apache-2.0
-from aws_cdk import (
-    Stack,
-    aws_s3 as s3,
-    aws_iam as iam,
-    RemovalPolicy,
-    Duration
-)
+from aws_cdk import Stack, aws_s3 as s3, aws_iam as iam, RemovalPolicy, Duration
 from constructs import Construct
-from .ecs_stack import EcsStack
 
 
 class S3Stack(Stack):
-    def __init__(self, scope: Construct, construct_id: str, ecs_stack: EcsStack, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         # Create S3 bucket with infrequent access
         self.bucket = s3.Bucket(
-            self, "OmiBucket",
+            self,
+            "OmiBucket",
             bucket_name="omi-data-bucket",
             versioned=True,
             encryption=s3.BucketEncryption.S3_MANAGED,
@@ -27,28 +21,9 @@ class S3Stack(Stack):
                     transitions=[
                         s3.Transition(
                             storage_class=s3.StorageClass.INFREQUENT_ACCESS,
-                            transition_after=Duration.days(30)
+                            transition_after=Duration.days(30),
                         )
                     ]
                 )
-            ]
-        )
-
-        # Create IAM policy for bucket access
-        bucket_policy = iam.PolicyStatement(
-            effect=iam.Effect.ALLOW,
-            actions=[
-                "s3:GetObject",
-                "s3:PutObject",
-                "s3:ListBucket",
-                "s3:DeleteObject"
             ],
-            resources=[
-                self.bucket.bucket_arn,
-                f"{self.bucket.bucket_arn}/*"
-            ]
         )
-
-        # Add the S3 policy to both ECS services' task roles
-        ecs_stack.backend_service.task_definition.task_role.add_to_policy(bucket_policy)
-        ecs_stack.frontend_service.task_definition.task_role.add_to_policy(bucket_policy)
