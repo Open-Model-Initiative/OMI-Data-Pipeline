4b88519c10a5e91aab7ba855b4493538909fab7b
jphillips <josh.phillips@fearnworks.com>
2025-02-13 17:34:50 +0000

    Remove odr_caption module
    Signed-off-by: jphillips <josh.phillips@fearnworks.com>

--- modules/odr_caption/endpoints-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-# SPDX-License-Identifier: Apache-2.0
-#!/bin/bash
-
-# Start the FastAPI server with uvicorn
-uvicorn odr_caption.server.app:app \
-    --host "0.0.0.0" \
-    --port 32100 \
-    --reload
