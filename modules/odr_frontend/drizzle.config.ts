bf5349acdf706b02f76681fa7a7ce25cf80eb2a4
jphillips <josh.phillips@fearnworks.com>
2025-02-19 11:49:29 -0600

    init frontend db struct
    Signed-off-by: jphillips <josh.phillips@fearnworks.com>

---
+++ modules/odr_frontend/drizzle.config.ts
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+
+import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
+export default defineConfig({
+  out: './drizzle',
+  schema: './src/db/schema.ts',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+});
