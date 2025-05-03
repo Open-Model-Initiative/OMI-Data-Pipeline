89197c13d43271554b78b76b9d797deca02e602e
Zachary Licastro <CheesyLaZanya@proton.me>
2025-05-02 23:17:33 -0400

    Push big version updates and initial fixes.
    Signed-off-by: Zachary Licastro <CheesyLaZanya@proton.me>

--- modules/odr_frontend/vite.config.ts
+++ modules/odr_frontend/vite.config.ts
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: Apache-2.0
-import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 
 export default defineConfig({
-	plugins: [sveltekit(), purgeCss()],
+	plugins: [sveltekit()],
 	server: {
 		fs: {
 			allow: ['/app/uploads']
