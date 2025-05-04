017fca2edc2957f918c7a1d491e3bb05b88f1480
Zachary Licastro <CheesyLaZanya@proton.me>
2025-05-04 19:14:45 -0400

    Split DCO into components.
    Signed-off-by: Zachary Licastro <CheesyLaZanya@proton.me>

---
+++ modules/odr_frontend/src/lib/dco/DCOCoverage.svelte
@@ -0,0 +1,14 @@
+<!--
+  SPDX-License-Identifier: Apache-2.0
+-->
+<div class="bg-primary-100 border-l-4 border-primary-500 text-primary-900 p-4 mb-8 rounded-lg">
+  <p>
+    The following Developer Certificate of Origin agreement covers all contributions to Open
+    Model Initiative Datasets. All OMI datasets will fall under the CDLA 2.0 license. See <a
+      href="https://cdla.dev/permissive-2-0/"
+      class="underline"
+      target="_blank"
+      rel="noopener noreferrer">https://cdla.dev/permissive-2-0/</a
+    > for details.
+  </p>
+</div>
