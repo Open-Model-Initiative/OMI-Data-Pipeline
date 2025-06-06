f381199622e3293dd6769488f8a247cda40af265
Zachary Licastro <CheesyLaZanya@proton.me>
2025-06-05 20:41:38 -0400

    Add initial attempt of footer build information.
    Signed-off-by: Zachary Licastro <CheesyLaZanya@proton.me>

---
+++ modules/odr_frontend/src/lib/footer/FooterBuild.svelte
@@ -0,0 +1,23 @@
+<!--
+  SPDX-License-Identifier: Apache-2.0
+-->
+<script>
+  import { buildInfo } from '$lib/buildInfo.js';
+</script>
+
+<div class="build-info">
+  <small>
+    Build: {buildInfo.branch}@{buildInfo.commit.slice(0, 7)}
+    | {buildInfo.buildTime}
+    | v{buildInfo.version}
+  </small>
+</div>
+
+
+<style>
+  .build-info {
+    opacity: 0.6;
+    font-size: 0.8em;
+    padding-top: 6px;
+  }
+</style>
