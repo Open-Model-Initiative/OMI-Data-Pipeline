5111781b586dd9cb968f218de999b94b7ef24fd5
Zachary Licastro <CheesyLaZanya@proton.me>
2025-06-06 20:54:07 -0400

    Remove debug logs, try origin env variable.
    Signed-off-by: Zachary Licastro <CheesyLaZanya@proton.me>

--- modules/odr_frontend/src/hooks.server.ts
+++ modules/odr_frontend/src/hooks.server.ts
@@ -6,12 +6,6 @@ import { sequence } from '@sveltejs/kit/hooks';
 const authorizationHandle:Handle = async ({ event, resolve }) => {
 	const session = await event.locals.auth();
 
-	console.log('AUTH_SECRET exists:', !!process.env.AUTH_SECRET);
-	console.log('GITHUB_CLIENT_ID exists:', !!process.env.GITHUB_CLIENT_ID);
-	console.log('GITHUB_CLIENT_SECRET exists:', !!process.env.GITHUB_CLIENT_SECRET);
-	console.log('DISCORD_CLIENT_ID exists:', !!process.env.DISCORD_CLIENT_ID);
-	console.log('DISCORD_CLIENT_SECRET exists:', !!process.env.DISCORD_CLIENT_SECRET);
-
 	if (event.url.pathname.startsWith('/auth')) {
 		return resolve(event);
 	} else if (!session) {
