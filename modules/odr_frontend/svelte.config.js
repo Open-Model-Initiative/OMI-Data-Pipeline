0af2e216622ffccd8fbda13d298a0f49a0600d45
Zachary Licastro <CheesyLaZanya@proton.me>
2025-06-25 18:22:33 -0400

    Remove invalid trustProxy setting and re-add csrf check origin disabled for now.
    Signed-off-by: Zachary Licastro <CheesyLaZanya@proton.me>

--- modules/odr_frontend/svelte.config.js
+++ modules/odr_frontend/svelte.config.js
@@ -18,7 +18,9 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-		trustProxy: true
+		csrf: {
+			checkOrigin: false,
+		},
 	},
 };
 export default config;
