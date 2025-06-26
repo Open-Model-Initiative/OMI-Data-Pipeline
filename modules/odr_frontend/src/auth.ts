3c1f453e149be9f8a8c5df7216de0a8505b2730b
Zachary Licastro <CheesyLaZanya@proton.me>
2025-06-25 21:33:01 -0400

    Skip db connection during build.
    Signed-off-by: Zachary Licastro <CheesyLaZanya@proton.me>

--- modules/odr_frontend/src/auth.ts
+++ modules/odr_frontend/src/auth.ts
@@ -12,20 +12,28 @@ import {
 import PostgresAdapter from '@auth/pg-adapter';
 import pg from 'pg';
 
-const pool = new pg.Pool({
-	host: process.env.POSTGRES_HOST,
-	user: process.env.POSTGRES_USER,
-	password: process.env.POSTGRES_PASSWORD,
-	database: process.env.POSTGRES_DB,
-	max: 20,
-	idleTimeoutMillis: 30000,
-	connectionTimeoutMillis: 2000
-});
+// Create pool only when not in build mode
+let pool: pg.Pool | undefined;
+
+function getPool() {
+	if (!pool && process.env.NODE_ENV !== 'build') {
+		pool = new pg.Pool({
+			host: process.env.POSTGRES_HOST,
+			user: process.env.POSTGRES_USER,
+			password: process.env.POSTGRES_PASSWORD,
+			database: process.env.POSTGRES_DB,
+			max: 20,
+			idleTimeoutMillis: 30000,
+			connectionTimeoutMillis: 2000
+		});
+	}
+	return pool;
+}
 
 export const { handle, signIn, signOut } = SvelteKitAuth(async (event) => {
 	console.log('X-Forwarded-Proto:', event.request.headers.get('x-forwarded-proto'));
-  console.log('X-Forwarded-Host:', event.request.headers.get('x-forwarded-host'));
-  console.log('Host:', event.request.headers.get('host'));
+	console.log('X-Forwarded-Host:', event.request.headers.get('x-forwarded-host'));
+	console.log('Host:', event.request.headers.get('host'));
 
 	const authOptions = {
 		providers: [
@@ -53,7 +61,7 @@ export const { handle, signIn, signOut } = SvelteKitAuth(async (event) => {
 		secret: AUTH_SECRET,
 		trustHost: true,
 		useSecureCookies: process.env.NODE_ENV === 'production',
-		adapter: PostgresAdapter(pool),
+		adapter: getPool() ? PostgresAdapter(getPool()!) : undefined,
 		callbacks: {
 			session({ session, user }: { session: Session; user: User }) {
 				session.user = user;
